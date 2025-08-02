86b696b1f850f0cb735cd7a69faa535babf87730
Parsa Ahmadi Nasab Emran <parsatempleuniversity@outlook.com>
2025-08-02 16:59:19 +0000

    Upadated AdvancedCounter.tsx by removing the unused useEffect import from 'react'

--- vite-project/src/components/AdvancedCounter/AdvancedCounter.tsx
+++ vite-project/src/components/AdvancedCounter/AdvancedCounter.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useCallback } from 'react';
 
 // Import child components and hooks
 import CounterDisplay from '../CounterDisplay/CounterDisplay';
